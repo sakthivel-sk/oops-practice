a9ad7d3305de1bb9f2419f92e8bf57a667d439d6
Sakthivel <sakthivkl@gmail.com>
2023-02-12 20:44:08 +0530

    adding files

--- src/com/sakthi/staticExample/Human.java
+++ src/com/sakthi/staticExample/Human.java
@@ -5,12 +5,19 @@ public class Human {
     String name;
     int salary;
     boolean married;
-    long population;
+    static long population;
+
+
+
+    static void message() {
+        System.out.println("Helllo World1");
+    }
+
     public Human(int age, String name, int salary, boolean married) {
         this.age = age;
         this.name = name;
         this.salary = salary;
         this.married = married;
-        this.population += 1;
+        Human.population += 1;
     }
 }
--- src/com/sakthi/staticExample/Main.java
+++ src/com/sakthi/staticExample/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Human sakthi = new Human(24, "Sakthivel-82",30000, false);
         Human deepak = new Human(32, "Deepak",40000, true);
+        Human sibi = new Human(32, "Sibi",40000, true);
 
-        System.out.println(sakthi.population);
-        System.out.println(deepak.population);
+        System.out.println(Human.population);
+
     }
 }
